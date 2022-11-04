9fbd33f2f7ff96cf03c8b43e5b604da0fe950e4d
Yang <yangah9991@gmail.com>
2022-11-04 22:11:37 +0800

    ignore stuff and add stuff to premake

--- premake5.lua
+++ premake5.lua
@@ -1,21 +1,29 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "off"
 
- 	targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
-    objdir ("Intermediate/" .. outputdir .. "/%{prj.name}")
+ 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
 
  	files
 	{
         "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
     }
 
  	filter "system:windows"
@@ -23,17 +31,18 @@ project "GLFW"
         systemversion "latest"
         staticruntime "On"
 
-         files
+        files
         {
             "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
         }
 
  		defines 
@@ -42,5 +51,18 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
 		}
 
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
+        
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
