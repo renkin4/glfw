c9106ee9d8175a932e9790bf9e28bb72be2a4cb4
weiyang-weike <119039658+weiyang-weike@users.noreply.github.com>
2023-09-10 08:45:45 +0800

    Premake update

--- premake5.lua
+++ premake5.lua
@@ -1,14 +1,14 @@
 project "GLFW"
-    kind "StaticLib"
-    language "C"
-    staticruntime "off"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
 
- 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
- 	files
+	files
 	{
-        "include/GLFW/glfw3.h",
+		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
 		"src/context.c",
@@ -24,16 +24,38 @@ project "GLFW"
 		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
-    }
+	}
 
- 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "latest"
-        staticruntime "On"
+	filter "system:linux"
+		pic "On"
 
-        files
-        {
-            "src/win32_init.c",
+		systemversion "latest"
+		
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		files
+		{
+			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_module.c",
 			"src/win32_monitor.c",
@@ -43,15 +65,20 @@ project "GLFW"
 			"src/wgl_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c"
-        }
-
- 		defines 
-		{ 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
 		}
 
-    filter "configurations:Debug"
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+		links
+		{
+			"Dwmapi.lib"
+		}
+
+	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
@@ -63,6 +90,16 @@ project "GLFW"
 		runtime "Release"
 		optimize "on"
         symbols "off"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+ 
+	filter "configurations:Debug_Dev"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release_Dev"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist_Dev"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
